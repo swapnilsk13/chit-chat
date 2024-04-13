4a007671ff65b33180dd99405d3e46e37eb24615
swapnil <swapnilsk999@gmail.com>
2024-04-13 14:06:03 +0530

    app.get added

--- server/index.js
+++ server/index.js
@@ -1,47 +1,52 @@
-//3rd party packages
+// 3rd party packages
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-//core modules
+// Core modules
 const bodyParser = require("body-parser");
 
-//own modules
+// Own modules
 const users = require("./routes/users");
 const messages = require("./routes/messages");
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080; // Corrected from process.env.port
 const mongodbURI = process.env.MONGOOSE_URI;
+
+// Server setup
 const server = app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
+// Socket.io setup
 const socketIo = require("socket.io")(server, { cors: { origin: "*" } });
 
-//Database Configuration
+// Database Configuration
 mongoose
   .connect(mongodbURI)
-  .then(() => console.log("Mongodb connected Succesfully"))
+  .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.log(err));
 
+// Middleware
 app.use(
   cors({
     origin: "https://chit-chat-client-mu.vercel.app", // Set the origin to your client's URL
   })
 );
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Assign socket object to every request
+// Assign socket object to every request
 app.use(function (req, res, next) {
   req.io = socketIo;
   next();
+});
+
+// Route handlers
+app.get("/", (req, res) => {
+  res.send("Hello from Chit Chat Server!"); // Example response
 });
 
 app.use("/users", users);
