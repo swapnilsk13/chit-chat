1cc0f7c14530aac8b381a032bb2362f056b73711
swapnil <swapnilsk999@gmail.com>
2024-04-13 14:35:28 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -30,11 +30,15 @@ mongoose
   .catch((err) => console.log(err));
 
 // Middleware
-app.use(
-  cors({
-    origin: "https://chit-chat-client-mu.vercel.app", // Set the origin to your client's URL
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://chit-chat-client-mu.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
